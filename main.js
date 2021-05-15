cd764eb519ee82277e8f40253f3d53615c95839b
kidwen <xuqiujia2020@outlook.com>
2021-05-16 00:00:53 +0800

    add self starting

--- main.js
+++ main.js
@@ -1,29 +1,30 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-function createWindow(){
+function createWindow() {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
-        webPreferences:{
-            preload: path.join(__dirname,'preload.js'),
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
         },
     });
     win.loadFile('index.html');
 }
 
-app.whenReady().then(()=>{
+app.whenReady().then(() => {
     createWindow();
 
-    app.on('active',()=>{
-        if(BrowserWindow.getAllWindows().length === 0){
+    app.on('active', () => {
+        app.setLoginItemSettings();
+        if (BrowserWindow.getAllWindows().length === 0) {
             createWindow();
         }
     });
 });
 
-app.on('window-all-closed',()=>{
-    if(process.platform !== 'darwin'){
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
         app.quit();
     }
 });
